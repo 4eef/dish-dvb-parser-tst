a351d632e8703be13a96974aa2a7f35b57fc666d
Vadym Baidachenko <4eef@ukr.net>
2019-02-27 18:01:10 +0200

    open() function used to work with files. Seems working.

--- inc/file.h
+++ inc/file.h
@@ -1,10 +1,26 @@
 #ifndef FILE_H
 #define FILE_H
 
+/* Includes */
 #include <stdint.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-void file_readFile(size_t *pFileStart);
+/* Defines */
+#define NO_ERROR                        0
+#define NO_SUCH_FILE                    1
+#define NO_FILE_TO_CLOSE                2
+#define ERROR_CLOSING_FILE              3
+#define BUFFER_PTR_ERROR                4
+
+/* Function prototypes */
+uint8_t file_openFile(char *pFilePath);
+uint8_t file_closeFile(void);
+uint8_t file_readToBuff(void *pBuff, uint16_t cnt);
+void file_testRead(void);
 
 #endif /* FILE_H */
 
--- src/file.c
+++ src/file.c
@@ -1,6 +1,65 @@
 #include "file.h"
 
-void file_readFile(size_t *pFileStart){
-    
+/* Memory */
+static int fileDscr;
+static uint32_t readCnt;
+
+/* Open a file to read... */
+uint8_t file_openFile(char *pFilePath){
+    /* Perform file open */
+    fileDscr = open(pFilePath, O_RDONLY);
+    /* Check the success */
+    if(fileDscr == -1){
+        printf("Error with opening file...\n");
+        return NO_SUCH_FILE;
+    }else{
+        printf("File is opened, reading...\n");
+    }
+    return NO_ERROR;
+}
+
+/* Close a file... */
+uint8_t file_closeFile(void){
+    int status;
+    /* Check file descriptor */
+    if(fileDscr <= 0){
+        printf("There is nothing to close!\n");
+        return NO_FILE_TO_CLOSE;
+    }
+    /* Close file */
+    status = close(fileDscr);
+    if(status != 0){
+        printf("Error closing file!\n");
+        return ERROR_CLOSING_FILE;
+    }
+    printf("File is closed.\n");
+    return NO_ERROR;
+}
+
+/* Read portion of data from file and write into buffer */
+uint8_t file_readToBuff(void *pBuff, uint16_t cnt){
+    /* Check for success */
+    if(fileDscr <= 0){
+        printf("Nothing to read. Open a file first!\n");
+        return NO_SUCH_FILE;
+    }else if(NULL == pBuff){
+        printf("Error with file read buffer pointer...\n");
+        return BUFFER_PTR_ERROR;
+    }
+    /* Perform file read */
+    readCnt += read(fileDscr, pBuff, cnt);
+    //readCnt += cnt;
+    printf("Read bytes: %i\n", readCnt);
+    return NO_ERROR;
+}
+
+/* File read test */
+void file_testRead(void){
+    uint8_t num;
+    char txtStr[50];
+
+    num = read(fileDscr, &txtStr[0], 1);
+
+    printf("first char is %c, num data is %i\n", txtStr[0], num);
 }
 
