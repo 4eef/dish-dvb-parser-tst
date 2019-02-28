bea5b18951020bafd8c42b2bafc97bdd91d463dc
Vadym Baidachenko <4eef@ukr.net>
2019-02-28 17:59:32 +0200

    Get section procedure implemented, but not working correctly: can't find needed PID on whole file

--- inc/parser.h
+++ inc/parser.h
@@ -3,6 +3,7 @@
 
 /* Includes */
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include "file.h"
@@ -14,10 +15,11 @@
 #define PACKET_GET_SUCCESS              0
 #define PACKET_READ_ERROR               1
 #define PACKET_NO_SYNC_BYTE             2
-
-/* Typedefs */
+#define POINTER_ERR                     3
+#define SECTION_GET_READY               4
 
 /* Function prototypes */
-uint8_t parser_getPacket(elmntryPckt_type *pElmntryPckt);
+uint8_t parser_getAnyPacket(elmntryPckt_type *pElmntryPckt);
+uint8_t parser_getAnySection(void *pRxBuff, ePidVals_type pidValue);
 
 #endif /* PARSER_H */
--- src/parser.c
+++ src/parser.c
@@ -2,11 +2,21 @@
 
 /* Memory */
 
+/* Inner mechanisms function prototypes */
+void doSomething(void);
+
+
+/* Interface functions */
 
 /* Reads input file to get a single 188-byte packet */
-uint8_t parser_getPacket(elmntryPckt_type *pElmntryPckt){
+uint8_t parser_getAnyPacket(elmntryPckt_type *pElmntryPckt){
     uint8_t status, readBuff[PACKET_LENGTH_BYTE] = {};
     uint16_t i = 0;
+    /* Check argument pointer */
+    if(NULL == pElmntryPckt){
+        printf("Error with packet pointer!\n");
+        return POINTER_ERR;
+    }
     /* Find the synchronization byte */
     while(1){
         status = file_readToBuff(&readBuff[0], 1);
@@ -20,6 +30,7 @@ uint8_t parser_getPacket(elmntryPckt_type *pElmntryPckt){
         /* Out of reading attempts */
         if(i >= BYTES_TO_READ){
             printf("Can't find packet synchronization byte!\n");
+            file_closeFile();
             return PACKET_NO_SYNC_BYTE;
         }
         /* Check for synchronization byte */
@@ -31,5 +42,57 @@ uint8_t parser_getPacket(elmntryPckt_type *pElmntryPckt){
     }
     memcpy(pElmntryPckt, &readBuff[0], PACKET_LENGTH_BYTE);
     return PACKET_GET_SUCCESS;
+}
+
+/* Get table section by table PID */
+uint8_t parser_getAnySection(void *pRxBuff, ePidVals_type pidValue){
+    bool pcktRxing = false;
+    uint8_t rxStat = 0;
+    uint16_t sctnLngth = 0;
+    uint32_t pcktsSkipped = 0;
+    elmntryPckt_type rxPckt = {0};
+    /* Check argument pointer */
+    if(NULL == pRxBuff){
+        printf("Error with packet pointer!\n");
+        return POINTER_ERR;
+    }
+    while(1){
+        /* Get single packet */
+        rxStat = parser_getAnyPacket(&rxPckt);
+        /* Filter it to needed identifier */
+        if(rxPckt.packetID == pidValue){
+            /* Start control */
+//            if((rxPckt.pldUnitStrtInd == 1) && (pcktRxing == false)){
+                pcktRxing = true;
+                /* Allocate memory to copy needed amount of data */
+                sctnLngth = ((rxPckt.data[(rxPckt.data[0] + 1)] << 8) | rxPckt.data[(rxPckt.data[0] + 2)]) & SECTION_LENGTH_MASK;
+                printf("Section length is %i\n%i packets skipped.\n", sctnLngth, pcktsSkipped);
+                return SECTION_GET_READY;
+//            }
+            /* Add data to  */
+            switch(pidValue){
+                case pidNit:
+                    break;
+                default:
+                    break;
+            }
+            /* End control */
+            if((rxPckt.pldUnitStrtInd == 1) && (pcktRxing == true)){
+                pcktRxing = false;
+                /* Done copying data */
+                break;
+            }
+        }
+        pcktsSkipped++;
+    }
+    return SECTION_GET_READY;
+}
+
+
+/* Inner functions */
+
+/* Do something */
+void doSomething(void){
+
 }
 
